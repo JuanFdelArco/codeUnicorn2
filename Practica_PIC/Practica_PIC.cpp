eab0ef214a814357c4d2a43a558ad8dc4f9ada27
JuanFdelArco <juanfdelarco@gmail.com>
2017-01-08 01:55:42 +0100

    Primer código funcional 100% de Aino
    PUTA AMA!!!!

--- Practica_PIC/Practica_PIC.cpp
+++ Practica_PIC/Practica_PIC.cpp
@@ -7,28 +7,166 @@
 using namespace cv;
 using namespace std;
 
+double comparar(Mat alfabeto, Mat img_tratada)
+{
+
+	cout << "vuelvo a entrar" << endl;
+	double matchresult = 100;
+	double mincontour = 200;
+	int CVCONTOUR_APPROX_LEVEL = 9;
+	Mat drawing = alfabeto.clone();
+	
+	bool nuevo1, nuevo2;
+
+	Mat bordes_alfabeto(alfabeto.size().width, alfabeto.size().height, CV_8UC1);
+	Mat bordes_tratada(img_tratada.size().width, img_tratada.size().height, CV_8UC1);
+
+	threshold(alfabeto, bordes_alfabeto, 127, 255, THRESH_BINARY);
+	imshow("prueba", bordes_alfabeto);
+	threshold(img_tratada,bordes_tratada, 127, 255, THRESH_BINARY);
+	cout << "mmm1:  " << matchresult << endl;
+
+	vector<vector<Point> >contours_alfabeto;
+	vector<vector<Point> >contours_tratada;
+	
+	vector<Vec4i>hierarchy_alfabeto;
+	vector<Vec4i>hierarchy_tratada;
+
+	vector<Point>nueva_secuencia_1;
+	vector<Point>nueva_secuencia_2;
+	vector<Point> boundary;
+	vector<Point> boundary2;
+	int c_size = 0, c_idx = 0;
+
+	findContours(bordes_alfabeto, contours_alfabeto, hierarchy_alfabeto, RETR_LIST, CHAIN_APPROX_SIMPLE);
+	//drawContours(drawing, contours_alfabeto, 1, (0, 0, 255), 2, 8, hierarchy_alfabeto, 0, Point());
+	//imshow("Contorno", drawing);
+
+	findContours(bordes_tratada, contours_tratada, hierarchy_tratada, RETR_LIST, CHAIN_APPROX_SIMPLE);
+	cout << "mmmm2: " << matchresult << endl;
+	//cout << "El tama駉 es" << contours_alfabeto.size() << endl;
+	//cout << "El tama駉 2 es" << contours_tratada.size() << endl;
+
+	
+
+		for (unsigned i = 0; i < contours_alfabeto.size(); i++)
+		{
+			boundary = contours_alfabeto[i];
+			cout << "bound" << boundary << endl;
+			nuevo1 = false;
+			if (arcLength(boundary, true) > mincontour)
+			{
+				approxPolyDP(boundary, nueva_secuencia_1, 2, true);
+				nuevo1 = true;
+			}
+			
+
+	}
+
+
+
+		for (int i = 0; i < contours_tratada.size(); i++)
+		{
+			boundary2 = contours_tratada[i];
+			//cout << "" << boundary2 << endl;
+			cout << " Perimetro: " << arcLength(boundary2, true) << endl;
+			nuevo2 = false;
+			if (arcLength(boundary2, true) > mincontour)
+			{
+				cout << "Estoy dentro sin problemas" << endl;
+				approxPolyDP(boundary2, nueva_secuencia_2, 2, true);
+				nuevo2 = true;
+
+			}
+
+		}
+
+		
+
+		//cout << "Nueva secuencia 1: " << nueva_secuencia_1 << endl;
+		//cout << "Nueva secuencia 2: " << nueva_secuencia_2 << endl;
+
+		cout << "antes del if" <<matchresult<< endl;
+		if (nuevo1 == true && nuevo2 == true)
+		{
+			cout << "nueva secuencia2:  " << nueva_secuencia_2 << endl;
+			cout << "nueva secuencia 1" << nueva_secuencia_1 << endl;
+
+			
+			matchresult = matchShapes(nueva_secuencia_2, nueva_secuencia_1, 2, 0);
+			//cout << "mmmm3: " << matchresult << endl;
+		}
+		cout << "Match:" << matchresult << endl;
+		bordes_alfabeto.release();
+		bordes_tratada.release();
+		contours_alfabeto.clear();
+		contours_tratada.clear();
+		
+	    
+		return matchresult;
+
+
+		
+
+
+	
+
+
+
+
+
+
+}
+
 
 int main(int argc, const char** argv)
 {
-	VideoCapture cam(1); // (0 = Camara del pc ; 1 = Camara externa)
+	vector<Mat> letras;
+	vector<Mat> resultados;
+	Mat letra_A,letra_B;
+	Mat res_A;
+	letra_A= imread("A.JPG", CV_LOAD_IMAGE_GRAYSCALE);
+	letras.push_back(letra_A);
+	//letra_B= imread("B.JPG", CV_LOAD_IMAGE_GRAYSCALE);
+	//letras.push_back(letra_B);
+	res_A = imread("resultado_A.jpg", CV_LOAD_IMAGE_COLOR);
+	resultados.push_back(res_A);
+
+	
+
+	
+	
+	
+	
+
+	
+	VideoCapture cam(0);
+	
 	if (!cam.isOpened()) {
 		cout << "ERROR not opened " << endl;
 		return -1;
 	}
+	
 	Mat img;
 	Mat img_threshold;
 	Mat img_gray;
 	Mat img_roi;
 	Mat capturada;
 
-//	namedWindow("Original_image", CV_WINDOW_AUTOSIZE);
-//	namedWindow("Gray_image", CV_WINDOW_AUTOSIZE);
-//	namedWindow("Thresholded_image", CV_WINDOW_AUTOSIZE);
-//	namedWindow("ROI", CV_WINDOW_AUTOSIZE);
+	double coincidencia; 
 
-	char a[40];
+	namedWindow("Original_image", CV_WINDOW_AUTOSIZE);
+	namedWindow("Gray_image", CV_WINDOW_AUTOSIZE);
+	namedWindow("Thresholded_image", CV_WINDOW_AUTOSIZE);
+	namedWindow("ROI", CV_WINDOW_AUTOSIZE);
+	
+
+	//char a[40];
 	int count = 0;
 	CvPoint pt1, pt2;
+	//lImage * tableauxImage[6];
+	
+
 
 	while (1) {
 		bool b = cam.read(img);
@@ -38,142 +176,33 @@ int main(int argc, const char** argv)
 		}
 		Rect roi(340, 100, 270, 270);
 		rectangle(img, roi, 2, 10, 8);
+
 		imshow("Original_image", img);
+
 		img_roi = img(roi);
-
-
 		if (waitKey(30) == 27) {
 			capturada = img_roi.clone();
 			cvtColor(capturada, img_gray, CV_RGB2GRAY);
 			GaussianBlur(img_gray, img_gray, Size(19, 19), 0.0, 0);
-			threshold(img_gray, img_threshold, 127, 255, THRESH_BINARY + THRESH_OTSU);
-			//Canny(img_gray, img_threshold, 100, 200, 3);
-
+			threshold(img_gray, img_threshold, 127, 255, THRESH_BINARY);
 			imshow("Threshold", img_threshold);
-			
-			//Valor de intensidad de los pixeles
-			//Esto estaba en el programa anterior, funciona, no se si serviran de algo
-			float cx = 0.0, cy = 0.0;
-			float sumi = 1.0;
-			for (int i = 0; i < img_threshold.rows; i++) {
-				for (int j = 0; j < img_threshold.cols; j++) {
-					cy += img_threshold.at<uchar>(i, j) * i;
-					cx += img_threshold.at<uchar>(i, j) * j;
-					sumi += img_threshold.at<uchar>(i, j);
-				}
-			}
-			cx /= sumi;
-			cy /= sumi;
-			cout << "Sumi: " << sumi << endl;
 
-			//Encontrar contornos
-			vector<vector<Point> >contours;
-			vector<Vec4i>hierarchy;
-			Mat temp = img_threshold.clone();
-			findContours(temp, contours, hierarchy, RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point());
-			
-
-			//Calculamos el mayor valor en el vector contours y su posicion
-			int c_size = 0, c_idx = 0;
-			Mat drawing = capturada.clone();
-			for (int i = 0; i < contours.size(); i++) {
-				if (contours.size() > c_size) { 
-					c_idx = i;
-					c_size = contours.size();
-				}
-				if (i != 0) {	//Dibujamos el contorno, quitamos el primer valor porque es el recuadro de la imagen
-					drawContours(drawing, contours, i, (0, 0, 255), 2, 8, hierarchy, 0, Point());
+			for (int i = 0; i<letras.size(); i++)
+			{
+				coincidencia = comparar(letras[i],img_threshold);
+				if (coincidencia < 2.1)
+				{
+					
+					imshow("Resultado", resultados[i]);
 				}
 			}
 
-			//Pasamos el mayor valor al vector boundary0 (es el rectagulo)
-			vector<Point> boundary0;
-			//Acumulamos el vector que se dibuja en una nueva variable
-			vector<Point> boundary1;
-			vector<Point> hull;
-			bool hull2;
-			if (!contours.empty()) {
-				boundary0 = contours[c_idx]; 
-				boundary1 = contours[1];
-			}
-
-
-			convexHull(boundary1,hull,true,true);
-			hull2 = isContourConvex(boundary1);
-			cout << "Convex " << hull << endl;
-			cout << "Convex " << hull2 << endl;
-			cout << "Area " << contourArea(boundary1) << endl;
-			cout << "Perimetro " << arcLength(boundary1,true) << endl;
-
-
-			//NO ENTIENDO BIEN ESTA FUNCION
-			//Aproximamos el contorno y lo dibujamos
-			vector<Point> boundary;
-			if (!boundary0.empty()) {
-				approxPolyDP(boundary1, boundary, 2, false); //Aproxima el valor del contorno
-				for (int i = 0; i < boundary.size(); i++) {
-					circle(capturada, boundary[i], 1, Scalar(0, 255, 0));
-				}
-			}
-
-
-			//Comprobamos la orientacion de la mano
-			int orientation = 0;
-			vector<Point> peaks_;
-			vector<Point> peaks0;
-			//ESTE BUCLE NO FUNCIONA! MALDITO BUCLE! - PERO ES MUY INTERESANTE
-			for (int i = 0; i<img_threshold.cols; i++) {
-				cout << "Bucle 1: " << i << endl;
-				cout << "	rows = " << img_threshold.rows << endl;
-				cout << "	cols = " << img_threshold.cols << endl;
-				
-				if (img_threshold.at<uchar>(img_threshold.rows - 1, i) > 0) {
-					cout << "	1. at = " << (int)img_threshold.at<uchar>(img_threshold.rows - 1, i) << endl;
-					orientation = 0;
-					break;
-				}
-				else if (img_threshold.at<uchar>(i, img_threshold.cols - 1) > 0) {
-					cout << "	2. at = " << (int)img_threshold.at<uchar>(i, img_threshold.cols - 1) << endl;
-					orientation = 1;
-					break;
-				}
-			}
-			cout << "Orientacion: " << orientation << endl;
-
-			if (orientation == 0) {
-				for (int i = 0; (i + 1) < boundary1.size(); i++) {
-					Point p = boundary1[i];
-					if (cy > p.y) {
-						bool isPeak = true;
-						int indxl = (i - 1) % boundary1.size();
-						int indxr = (i + 1) % boundary1.size();
-						if (boundary1[indxl].y < p.y || boundary1[indxr].y < p.y) {
-							isPeak = false;
-						}
-						if (isPeak) {
-							peaks_.push_back(p);
-							circle(img_threshold, p + Point(roi.x, roi.y), 1, Scalar(255, 0, 0));
-						}
-					}
-				}
-			}
-			//Picos
-			for (int i = 0; i < peaks_.size(); i++) {
-				int nexti = (i + 1) % peaks_.size();
-				if (abs(peaks_[i].y - peaks_[nexti].y) < 15 && abs(peaks_[i].x - peaks_[nexti].x) < 15) {
-					continue;
-				}
-				peaks0.push_back(peaks_[i]);
-				circle(capturada, peaks_[i], 10, Scalar(0, 0, 255));
-				cout << "Picos "<< peaks_[i] << endl;
-			}
-
-				/// Show in a window
-				imshow("Contours", drawing);
-				imshow("Resultado", capturada);
-			}
+			imshow("Original_image", capturada);
+			imshow("Threshold", img_threshold);
 
 		}
-	return 0;
+	}
+				return 0;
+		}
 
-}
+
